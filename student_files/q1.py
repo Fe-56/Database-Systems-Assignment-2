b1c1a63bfe46c521262ed8bd2388f631e034ec4e
Fe-56 <limfuoen1999@gmail.com>
2024-04-08 01:49:37 +0800

    Complete Questions 1 & 2

--- student_files/q1.py
+++ student_files/q1.py
@@ -2,10 +2,32 @@ import sys
 from pyspark.sql import SparkSession
 # you may add more import if you need to
 
-
 # don't change this line
 hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 
+hdfs_path = "hdfs://%s:9000"
+
+# read the .csv
+hdfs_nn = "localhost"
+df = spark.read.option('header', True)\
+        .csv(f"{hdfs_path}/assignment2/part1/input/" % (hdfs_nn))
+
+# convert the "Number of Reviews" annd "Rating" columns to int and float respectively
+num_of_reviews = "Number of Reviews"
+rating = "Rating"
+df = df.withColumn(num_of_reviews, df[num_of_reviews].astype("int"))
+df = df.withColumn(rating,  df[rating].astype("float"))
+
+# clean the .csv
+cleaned_df = df.filter((df[num_of_reviews] > 0) & (df[rating] >= 1.0))
+
+cleaned_df.show()
+
+# write the .csv
+save_path = f"{hdfs_path}/assignment2/output/question1/" % (hdfs_nn)
+cleaned_df.write.format("csv").save(save_path)
+
+spark.stop()
--- student_files/q2.py
+++ student_files/q2.py
@@ -2,10 +2,70 @@ import sys
 from pyspark.sql import SparkSession
 
 # you may add more import if you need to
-
+from pyspark.sql.functions import max, min, col
 
 # don't change this line
 hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
+
+hdfs_path = "hdfs://%s:9000"
+
+# read the .csv
+hdfs_nn = "localhost"
+df = spark.read.option('header', True)\
+        .csv(f"{hdfs_path}/assignment2/part1/input/" % (hdfs_nn))
+
+# convert the "Rating" column to float
+rating = "Rating"
+city = "City"
+price_range = "Price Range"
+df = df.withColumn(rating,  df[rating].astype("float"))
+
+# ignore the rows where "Price Range" is null
+df = df.filter(df[price_range].isNotNull())
+
+# find the best and worst restaurants for each city for each price range
+best_restaurants = df.groupBy(city, price_range) \
+                     .agg(max(rating).alias(rating))
+worst_restaurants = df.groupBy(city, price_range) \
+                     .agg(min(rating)).alias(rating)
+
+# so that the worst restaurants are below the best restaurants
+resulting_df = best_restaurants.union(worst_restaurants)
+
+# join with the original df to get back the original schema
+resulting_df = resulting_df.join(df, [price_range, city, rating], how="inner")
+
+# remove any duplicate rows for "Price Range", "City", and "Rating", so that there is only row for a unique value of those columns
+resulting_df = resulting_df.dropDuplicates([price_range, city, rating])
+
+# rearrange the order of the columns
+resulting_df = resulting_df.select(
+  "_c0",
+  "Name",
+  city,
+  "Ranking",
+  rating,
+  price_range,
+  "Number of Reviews",
+  "Reviews",
+  "URL_TA",
+  "ID_TA"
+)
+
+# sort the columns
+resulting_df = resulting_df.sort(
+  col(city).asc(),
+  col(price_range).asc(),
+  col(rating).desc()
+)
+
+resulting_df.show()
+
+# write the .csv
+save_path = f"{hdfs_path}/assignment2/output/question2/" % (hdfs_nn)
+resulting_df.write.format("csv").save(save_path)
+
+spark.stop()
