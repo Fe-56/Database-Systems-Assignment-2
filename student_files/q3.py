2376ccbb6952e1c791f941a7ba4fc2ab6113ed18
Han Wei Guang <hanweiguang25@gmail.com>
2024-04-09 22:00:12 +0800

    complete q3 and q4

--- student_files/q3.py
+++ student_files/q3.py
@@ -9,3 +9,38 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
+hdfs_path = "hdfs://%s:9000"
+
+# read the .csv
+hdfs_nn = "localhost"
+df = spark.read.option('header', True)\
+        .csv(f"{hdfs_path}/assignment2/part1/input/" % (hdfs_nn))
+        
+
+rating = "Rating"
+city = "City"
+avg_rating = "avg(rating)"
+average_rating = "AverageRating"
+rating_group = "RatingGroup"
+top_rating = "Top"
+bottom_rating = "Bottom"
+# convert rating to float and remove null ratings
+df = df.withColumn(rating,  df[rating].astype("float"))
+df = df.filter(df[rating].isNotNull())
+
+# calculate the average rating for each city and rename column
+average_rating_by_city = df.groupBy(city).agg(avg(rating)).withColumnRenamed(avg_rating, average_rating)
+
+# get top 3 and bottom 3 cities by average rating and add rating group
+top_3_df = average_rating_by_city.orderBy(average_rating_by_city[average_rating].desc()).limit(3).withColumn(rating_group, lit(top_rating))
+bottom_3_df = average_rating_by_city.orderBy(average_rating_by_city[average_rating].asc()).limit(3).withColumn(rating_group, lit(bottom_rating))
+
+# combine top 3 and bottom 3 cities
+top_bottom_3_df = top_3_df.union(bottom_3_df)
+top_bottom_3_df.show()
+
+# write the .csv
+save_path = f"{hdfs_path}/assignment2/output/question3/" % (hdfs_nn)
+top_bottom_3_df.write.format("csv").save(save_path)
+
+spark.stop()
--- student_files/q4.py
+++ student_files/q4.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import col, explode, split, regexp_replace, count
 # you may add more import if you need to
 
 
@@ -9,3 +9,36 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 4").getOrCreate()
 # YOUR CODE GOES BELOW
+hdfs_path = "hdfs://%s:9000"
+
+# read the .csv
+hdfs_nn = "localhost"
+df = spark.read.option('header', True)\
+        .csv(f"{hdfs_path}/assignment2/part1/input/" % (hdfs_nn))
+        
+city = "City"
+cuisine_style = "Cuisine Style"
+cuisine = "Cuisine"
+cuisine_count = "count(Cuisine)"
+counts = "count"
+# Filter out rows where "Cuisine Style" is null
+df = df.filter(df[cuisine_style].isNotNull())
+# Remove brackets and single quotes from "Cuisine Style" column
+df_replace_brackets = df.withColumn(cuisine_style, regexp_replace(col(cuisine_style), r"[\[\]\'\']", ""))
+
+# Split the "Cuisine Style" column by comma and rename to cuisine   
+df_split = df_replace_brackets.withColumn(cuisine_style, split(df_replace_brackets[cuisine_style], ",").alias(cuisine))
+# Explode the "Cuisine Style" column so that there is only one cuisine per row
+df_exploded = df_split.withColumn(cuisine_style, explode(cuisine_style)).withColumnRenamed(cuisine_style, cuisine)
+
+# Group by "City" and "Cuisine" and count the number of restaurants
+restaurants_by_city_cuisine = df_exploded.groupBy(city, cuisine).agg(count(cuisine))
+# Rename the count column to "count"
+df_renamed = restaurants_by_city_cuisine.withColumnRenamed(cuisine_count, counts)
+df_renamed.show()
+
+# write the .csv
+save_path = f"{hdfs_path}/assignment2/output/question4/" % (hdfs_nn)
+df_renamed.write.format("csv").save(save_path)
+
+spark.stop()
